dba89103085165ca350dad777e11c740631c2bee
Surya <iamsuryavamsi@gmail.com>
2024-09-22 14:38:33 +0530

    Updated Loading Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,13 @@ import WhyAreWeUnique from './WhyWeAreUnique'
 import '../Components/CSS/Home.css'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 const Home = () => {
 
     const [heroSectionImg, setHeroSectionImg] = useState(false);
+
+    const [pageLoading, setPageLoading] = useState(true);
     
     const askUsAnything = (e) => {
 
@@ -35,7 +38,21 @@ const Home = () => {
 
         }
 
-        heroSectionImgFunction();
+        const intervalId = setInterval(heroSectionImgFunction, 2000);
+
+        setTimeout(intervalId, 5000);
+
+        // Runnin the page loading
+
+        const updatePageLoading = () => {
+
+            setPageLoading(!pageLoading);
+
+        }
+
+        const pageLoadingId = setInterval(updatePageLoading, 3000);
+
+        setTimeout(pageLoading, 5000);
 
     }, []);
 
@@ -43,97 +60,135 @@ const Home = () => {
 
         <>
 
-            <NavBar />
-        
-            <div className="w-full flex justify-center items-center max-mdCustom:flex-col ">
+            { !pageLoading && (
 
-                {heroSectionImg && (
+                    <>
 
-                    <div className="relative">
+                    <NavBar />
+                    
+                    <div className="w-full flex justify-center items-center max-mdCustom:flex-col ">
 
-                        <img 
-                            src='/Hero_Section/Hero_Section_Main.png'
-                            className='hero_main_img h-[700px] w-[700px] max-mdCustom:w-[600px] max-mdCustom:h-auto max-smCustom:w-[500px] max-smCustom:h-[400px]'
-                        />
+                        {heroSectionImg && (
 
-                        <img 
-                            src='/Common/pale_blue_right.png'
-                            className='blue_right w-[70px] max-smCustom:w-[40px] absolute top-[140px] right-10 max-mdCustom:right-6 max-mdCustom:top-[120px] rotate-[10deg] max-smCustom:top-[80px] max-smCustom:right-[23px] max-smCustom:rotate-[-1deg]'
-                        />
+                            <div className="relative">
+
+                                <img 
+                                    src='/Hero_Section/Hero_Section_Main.png'
+                                    className='hero_main_img h-[700px] w-[700px] max-mdCustom:w-[600px] max-mdCustom:h-auto max-smCustom:w-[500px] max-smCustom:h-[400px]'
+                                />
+
+                                <img 
+                                    src='/Common/pale_blue_right.png'
+                                    className='blue_right w-[70px] max-smCustom:w-[40px] absolute top-[140px] right-10 max-mdCustom:right-6 max-mdCustom:top-[120px] rotate-[10deg] max-smCustom:top-[80px] max-smCustom:right-[23px] max-smCustom:rotate-[-1deg]'
+                                />
+
+                            </div>
+
+                        )}
+
+                        <div className="relative max-smCustom:w-[70%]">
+
+                            <img 
+                                src='/Common/yellow_left.png'
+                                className='blue_right w-[70px] max-smCustom:w-[40px] absolute left-[-70px] top-[-40px] rotate-[-10deg] max-smCustom:left-[-40px] max-smCustom:top-[-20px]'
+                            />
+
+                            <div
+                                className='hero_desc'
+                            >
+
+                                <p
+                                    className='font-semibold text-[40px] max-smCustom:text-[25px]'
+                                >
+
+                                    Take your business to <br />
+                                    <span
+                                        className='text-paleGreen animate-pulse'
+                                    >next level </span>
+
+                                </p>
+
+                                <p
+                                    className='text-[18px] font-montSerrat max-smCustom:text-[15px]'
+                                >
+
+                                    Get a custom website and smart digital solutions <br />
+                                    using the latest tech to grab attention and make your <br />business grow.
+
+                                </p>
+
+                                <p
+                                    className='askUsAnything font-montSerrat text-[15px] bg-yellowCustom inline-block px-[10px] py-[7px] rounded-[10px] font-semibold mt-5 cursor-pointer'
+                                    onClick={(e) => askUsAnything(e)}
+                                >
+
+                                    <button
+                                    >Ask us anything</button>
+
+                                </p>
+                            </div>
+
+                        </div>
+
+                        { !heroSectionImg && (
+
+                            <div className="relative">
+
+                                <img 
+                                    src='/Hero_Section/Hero_Section_Main.png'
+                                    className='hero_main_img h-[700px] w-[700px]'
+                                />
+
+                                <img 
+                                    src='/Common/pale_blue_right.png'
+                                    className='blue_right w-[70px] absolute top-[140px] right-10'
+                                />
+
+                            </div>
+
+                        )}
 
                     </div>
 
-                )}
+                    <KeyServices />
 
-                <div className="relative max-smCustom:w-[70%]">
+                    <WhyAreWeUnique />
 
-                    <img 
-                        src='/Common/yellow_left.png'
-                        className='blue_right w-[70px] max-smCustom:w-[40px] absolute left-[-70px] top-[-40px] rotate-[-10deg] max-smCustom:left-[-40px] max-smCustom:top-[-20px]'
-                    />
+                    <Footer />
 
-                    <div
-                        className='hero_desc'
-                    >
+                    </>
 
-                        <p
-                            className='font-semibold text-[40px] max-smCustom:text-[25px]'
-                        >
+                )
 
-                            Take your business to <br />
-                            <span
-                                className='text-paleGreen animate-pulse'
-                            >next level </span>
+            }
 
-                        </p>
+            {pageLoading && (
 
-                        <p
-                            className='text-[18px] font-montSerrat max-smCustom:text-[15px]'
-                        >
+                <>
 
-                            Get a custom website and smart digital solutions <br />
-                            using the latest tech to grab attention and make your <br />business grow.
+                    <div className="w-full flex h-[1000px] justify-center items-center space-x-3 text-[20px]">
 
-                        </p>
+                        <div className="">
 
-                        <p
-                            className='askUsAnything font-montSerrat text-[15px] bg-yellowCustom inline-block px-[10px] py-[7px] rounded-[10px] font-semibold mt-5 cursor-pointer'
-                            onClick={(e) => askUsAnything(e)}
-                        >
+                            <AiOutlineLoading3Quarters 
+                                className='animate-spin'
+                            />
 
-                            <button
-                            >Ask us anything</button>
+                        </div>
 
-                        </p>
-                    </div>
+                        <div className="">
 
-                </div>
+                            Loading ...
 
-                { !heroSectionImg && (
-
-                    <div className="relative">
-
-                        <img 
-                            src='/Hero_Section/Hero_Section_Main.png'
-                            className='hero_main_img h-[700px] w-[700px]'
-                        />
-
-                        <img 
-                            src='/Common/pale_blue_right.png'
-                            className='blue_right w-[70px] absolute top-[140px] right-10'
-                        />
+                        </div>
 
                     </div>
 
-                )}
+                </>
 
-            </div>
+            )
 
-            <KeyServices />
-
-            <WhyAreWeUnique />
-
-            <Footer />
+            }
 
         </>
 
