55499c988c065b9dc43fc592f6f519bb3232cae3
Surya <iamsuryavamsi@gmail.com>
2024-09-24 09:08:12 +0530

    Completed Two Services

--- src/Components/Web_App_Development.jsx
+++ src/Components/Web_App_Development.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Footer from './Footer'
 import NavBar from './NavBar'
+import './CSS/WebDesign.css'
 
 const Web_App_Development = () => {
 
@@ -18,19 +19,25 @@ const Web_App_Development = () => {
         
             <div className="pt-[100px] w-full text-center">
 
-                <div className="bg-greenCustom inline-block px-5 py-2 rounded-2xl text-[25px] font-bold font-montSerrat mt-[50px]">
+                <div className=" inline-block px-5 mb-10 py-2 rounded-2xl text-[25px] font-bold font-montSerrat mt-[50px] max-smCustom:mt-[0px] relative max-smCustom:text-[25px]"> 
 
                     Overview
+
+                    <img 
+                    className='w-[50px] h-auto rotate-[110deg] absolute right-[-25px] top-[-10px] green_image'
+                    src='/Common/green-left.png'
+                />
+
 
                 </div>
 
             </div>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full flex justify-center bg-yellowCustom">
 
-                <div className="bg-yellowCustom my-[50px] w-[1400px] rounded-2xl pb-[50px] max-mdCustom:w-[800px]">
+                <div className="bg-white overViewButton my-[50px] w-[1400px] rounded-[30px] pb-[50px] max-mdCustom:w-[800px] max-smCustom:w-full max-smCustom:mx-10">
 
-                    <div className="flex justify-center items-center h-[400px] space-x-[300px] max-mdCustom:block max-mdCustom:text-center max-mdCustom:space-y-5 max-mdCustom:h-auto max-mdCustom:mt-[50px] max-mdCustom:items-center">
+                    <div className="flex justify-center items-center h-[400px] space-x-[300px] max-mdCustom:block max-mdCustom:text-center max-mdCustom:space-y-5 max-mdCustom:h-auto max-mdCustom:mt-[50px] max-mdCustom:items-center max-smCustom:space-x-0 max-smCustom:mx-10">
 
                         <img 
                             src='/Services/Website_Development.png'
@@ -38,13 +45,13 @@ const Web_App_Development = () => {
                         />
 
                         <p
-                            className='font-bold font-montSerrat text-[30px] w-[500px] max-mdCustom:relative right-[140px]'
+                            className='font-bold font-montSerrat text-[30px] w-[500px] max-mdCustom:relative right-[140px] max-smCustom:text-[22px] max-smCustom:static max-smCustom:w-full'
                         >
 
                                 Website & Web App Development<br />
                                 
                                 <span
-                                    className='text-[22px]'
+                                    className='text-[22px] max-smCustom:text-[16px]'
                                 >
 
                                     Builds with Top-Notch Technologies
@@ -55,10 +62,10 @@ const Web_App_Development = () => {
 
                     </div>
 
-                    <div className="space-y-10 mt-10 mx-20">
+                    <div className="space-y-10 mt-10 mx-20 max-smCustom:mx-5">
 
                         <p
-                            className='text-[22px] font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[16px] font-semibold font-inknutAntiqua'
                         >
 
                             Building a website or web app is more than just writing code—it's about crafting a dynamic digital solution that fuels your business. Our development process harnesses the latest tech to ensure your site or application is fast, secure, and scalable! <br /><br />
@@ -81,14 +88,14 @@ const Web_App_Development = () => {
 
             </div>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full my-20 flex justify-center bg-greenCustom">
 
-                <div className="bg-greenCustom my-[50px] w-[1400px] rounded-2xl pb-[50px] max-mdCustom:w-[800px]">
+                <div className="overViewButton bg-white my-[50px] w-[1400px] rounded-2xl pb-[50px] max-mdCustom:w-[800px] max-smCustom:w-full max-smCustom:mx-10  max-smCustom:space-x-0">
 
-                    <div className="space-y-10 mt-10 mx-20">
+                    <div className="space-y-10 mt-10 mx-20 max-smCustom:mx-5">
 
                         <p
-                            className='text-[22px] font-bold font-montSerrat'
+                            className='text-[22px] font-semibold font-montSerrat'
                         >
 
                             Our recipe for Website & Web App Development: <br /><br />
@@ -96,7 +103,7 @@ const Web_App_Development = () => {
                         </p>
 
                         <p
-                            className='text-[22px] font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[14px] font-inknutAntiqua'
                         >
 
                             1. Requirement Gathering
@@ -142,7 +149,7 @@ const Web_App_Development = () => {
                             <br /><br />
 
                             <span
-                                className='text-[22px] font-bold'
+                                className='text-[22px] font-montSerrat font-bold'
                             >
                                 
                                 - Code Quality Assurance
--- src/Components/Web_Design_Service.jsx
+++ src/Components/Web_Design_Service.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Footer from './Footer'
 import NavBar from './NavBar'
+import './CSS/WebDesign.css'
 
 const Web_Design_Service = () => {
 
@@ -8,43 +9,48 @@ const Web_Design_Service = () => {
 
         window.scrollTo(0, 0);
 
-    }, []);
+    }, []); 
 
     return (
 
         <>
 
-            <NavBar />
+            <NavBar /> 
         
             <div className="pt-[100px] w-full text-center">
 
-                <div className="bg-greenCustom inline-block px-5 py-2 rounded-2xl text-[25px] font-bold font-montSerrat mt-[50px]">
+                <div className=" inline-block px-5 py-2 rounded-[30px] text-[30px] max-smCustom:text-[25px] font-semibold font-montSerrat mt-[50px] max-smCustom:mt-[0px] relative">
 
                     Overview
+
+                <img 
+                    className='w-[50px] h-auto rotate-[110deg] absolute right-[-25px] top-[-10px] green_image'
+                    src='/Common/green-left.png'
+                />
 
                 </div>
 
             </div>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full flex justify-center bg-yellowCustom ">
 
-                <div className="bg-yellowCustom my-[50px] w-[1400px] rounded-2xl pb-[50px] max-mdCustom:w-[800px]">
+                <div className="my-[50px] overViewButton bg-white w-[1400px] rounded-[30px] pb-[50px] max-mdCustom:w-[800px] max-smCustom:w-full max-smCustom:mx-10">
 
-                    <div className="flex justify-center items-center h-[400px] space-x-[300px] max-mdCustom:block max-mdCustom:text-center max-mdCustom:space-y-5 max-mdCustom:h-auto max-mdCustom:mt-[50px] max-mdCustom:items-center">
+                    <div className=" flex justify-center items-center h-[400px] space-x-[300px] max-mdCustom:block max-mdCustom:text-center max-mdCustom:space-y-5 max-mdCustom:h-auto max-mdCustom:mt-[50px] max-mdCustom:items-center max-smCustom:space-x-0 max-smCustom:mx-10">
 
                         <img 
                             src='/Services/Website_Desigining.png'
-                            className='w-[400px] max-mdCustom:mx-auto hover:w-[420px] transition-all duration-300'
+                            className='w-[400px] max-mdCustom:mx-auto transition-all duration-300'
                         />
 
                         <p
-                            className='font-bold font-montSerrat text-[48px] w-[500px] max-mdCustom:relative right-[140px]'
+                            className='font-bold font-montSerrat text-[48px] w-[500px] max-smCustom:w-full max-mdCustom:relative right-[140px] max-smCustom:text-[22px] max-smCustom:static'
                         >
 
                             Website / Web App Designing <br />
                             
                             <span
-                                className='text-[22px]'
+                                className='text-[22px] max-smCustom:text-[16px]'
                             >
                             
                                 Crafting Digital Experience That Captivate
@@ -55,10 +61,10 @@ const Web_Design_Service = () => {
 
                     </div>
 
-                    <div className="space-y-10 mt-10 mx-20">
+                    <div className="space-y-10 mt-10 mx-20 max-smCustom:mx-5">
 
                         <p
-                            className='text-[22px] font-bold font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[18px] font-bold font-montSerrat'
                         >
 
                             What is a Website and a Web App?
@@ -66,7 +72,7 @@ const Web_Design_Service = () => {
                         </p>
 
                         <p
-                            className='text-[22px] font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[16px] font-inknutAntiqua'
                         >
 
                         A website is a collection of related web pages accessible through the internet. It serves as an online presence for businesses, organizations, or individuals, and can include information like company details, product listings, or blog posts. Websites can range from simple single-page sites to complex platforms with many sections and features.
@@ -74,7 +80,7 @@ const Web_Design_Service = () => {
                         </p>
 
                         <p
-                            className='text-[22px] font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[16px] font-inknutAntiqua'
                         >
 
                             A web app (short for web application) is an interactive tool that performs specific functions online. Unlike traditional websites, web apps are designed to handle tasks like booking appointments, managing accounts, or filling out forms. They offer a more dynamic and engaging user experience.
@@ -82,45 +88,45 @@ const Web_Design_Service = () => {
                         </p>
 
                         <p
-                            className='text-[20px] font-montSerrat'
+                            className='text-[20px] max-smCustom:text-[16px] font-inknutAntiqua'
                         >
 
                             <span
-                                className='text-[22px] font-bold'
+                                className='text-[22px] font-semibold font-montSerrat'
                             >Designing for Impact</span> <br /><br />
 
 
-                            Creative Design: This aspect involves making your website or web app visually attractive and unique. It includes selecting colors, fonts, images, and layouts that not only catch the eye but also reflect your brand’s personality. Creative design helps your site stand out and make a memorable impression.<br /><br />
+                            <span className='font-montSerrat font-bold'>Creative Design:</span> This aspect involves making your website or web app visually attractive and unique. It includes selecting colors, fonts, images, and layouts that not only catch the eye but also reflect your brand’s personality. Creative design helps your site stand out and make a memorable impression.<br /><br />
 
-                            User-Friendly Design: A user-friendly design ensures that visitors can easily navigate and interact with your site or app. This means creating intuitive menus, clear call-to-action buttons, and a layout that works well on both computers and mobile devices. The goal is to make the user experience smooth and enjoyable, allowing users to find what they need quickly.
+                            <span className='font-montSerrat font-bold'>User-Friendly Design:</span> A user-friendly design ensures that visitors can easily navigate and interact with your site or app. This means creating intuitive menus, clear call-to-action buttons, and a layout that works well on both computers and mobile devices. The goal is to make the user experience smooth and enjoyable, allowing users to find what they need quickly.
 
                             <br /><br />
 
                             <span
-                                className='font-bold text-[22px]'
+                                className='font-semibold text-[22px] font-montSerrat'
                             >What Goes Behind the Design</span>
 
                             <br /><br />
 
-                            1. Understanding Needs: We begin by understanding your business goals and audience. This helps us design a site or app that meets your specific needs.
+                            <span className='font-montSerrat font-bold'>1. Understanding Needs:</span> We begin by understanding your business goals and audience. This helps us design a site or app that meets your specific needs.
 
                             <br /><br />
 
-                            2. Planning: We create wireframes or basic blueprints to plan the structure and functionality of the site or app.
+                            <span className='font-montSerrat font-bold'>2. Planning:</span> We create wireframes or basic blueprints to plan the structure and functionality of the site or app.
 
                             <br /><br />
 
-                            3. Design Creation: We then design detailed visual elements, incorporating your brand’s colors, fonts, and images to create a cohesive look.
+                            <span className='font-montSerrat font-bold'>3. Design Creation:</span> We then design detailed visual elements, incorporating your brand’s colors, fonts, and images to create a cohesive look.
 
                             <br /><br />
 
-                            4. Testing and Refinement: Finally, we test the design on different devices and browsers to ensure everything works properly and make any necessary adjustments.
+                            <span className='font-montSerrat font-bold'>4. Testing and Refinement: </span> Finally, we test the design on different devices and browsers to ensure everything works properly and make any necessary adjustments.
 
                             <br /><br />
 
 
                             <span
-                                className='text-[22px] font-bold'
+                                className='text-[22px] font-semibold max-smCustom:text-[16px] font-inknutAntiqua'
                             >
 
                             In summary, our website and web app design services aim to create visually engaging and easy-to-use platforms that reflect your brand and provide an excellent user experience.
@@ -135,14 +141,14 @@ const Web_Design_Service = () => {
 
             </div>
 
-            <div className="w-full flex justify-center">
+            <div className="w-full bg-greenCustom my-20 flex justify-center">
 
-                <div className="bg-greenCustom my-[50px] w-[1400px] rounded-2xl pb-[50px] max-mdCustom:w-[800px]">
+                <div className="overViewButton bg-white my-[50px] w-[1400px] rounded-[30px] pb-[50px] max-mdCustom:w-[800px] max-smCustom:w-full max-smCustom:mx-10  max-smCustom:space-x-0">
 
-                    <div className="space-y-10 mt-10 mx-20">
+                    <div className="space-y-10 mt-10 mx-20 max-smCustom:mx-5">
 
                         <p
-                            className='text-[22px] font-bold font-montSerrat'
+                            className='text-[22px] font-semibold font-montSerrat'
                         >
 
                             Our recipe for Website & Web App Design:
@@ -150,17 +156,17 @@ const Web_Design_Service = () => {
                         </p>
 
                         <p
-                            className='text-[22px] font-montSerrat'
+                            className='text-[22px] max-smCustom:text-[14px] font-inknutAntiqua'
                         >
 
-                            1. Discovery & Research: <br /> <br />
+                            <span className='font-montSerrat font-bold'>1. Discovery & Research:</span> <br /> <br />
 
                             What We Do: We start by getting to know your business, your goals, and your target audience. This involves discussions, questionnaires, and market research.
 
                             Why It's Important: Understanding your brand and audience ensures that the design resonates with your potential customers and meets your business objectives. <br /><br />
 
 
-                            2. Planning & Strategy: <br /> <br />
+                            <span className='font-montSerrat font-bold'>2. Planning & Strategy: <br /> <br /></span>
 
                             What We Do: We create a sitemap and wireframes – the blueprint of your website or web app. This outlines the structure, layout, and flow of each page or screen.
 
@@ -168,7 +174,7 @@ const Web_Design_Service = () => {
 
                             <br /> <br />
 
-                            3. Design Mockups: <br /> <br />
+                            <span className='font-montSerrat font-bold'>3. Design Mockups: <br /> <br /></span>
 
                             What We Do: We craft visual mockups based on the wireframes, incorporating your branding elements like colors, fonts, and logos. You'll get to see how your website or app will look before we start building it.
 
@@ -176,7 +182,7 @@ const Web_Design_Service = () => {
 
                             <br /> <br />
 
-                            4. Feedback & Revisions: <br /> <br />
+                            <span className='font-montSerrat font-bold'>4. Feedback & Revisions:</span> <br /> <br />
 
                             What We Do: We present the mockups to you and gather your feedback. Based on your input, we make necessary revisions to ensure the design meets your vision.
 
@@ -185,7 +191,7 @@ const Web_Design_Service = () => {
 
                             <br /> <br />
 
-                            5. Responsive Design: <br /> <br />
+                            <span className='font-montSerrat font-bold'>5. Responsive Design:</span> <br /> <br />
 
                             What We Do: We ensure that the design adapts seamlessly to different devices, whether it’s a desktop, tablet, or smartphone.
 
@@ -193,7 +199,7 @@ const Web_Design_Service = () => {
 
                             <br /><br />
 
-                            6. User Interface (UI) & User Experience (UX) Optimization: <br /> <br />
+                            <span className='font-montSerrat font-bold'>6. User Interface (UI) & User Experience (UX) Optimization:</span> <br /> <br />
 
                             What We Do: We focus on the details – button placements, forms, calls-to-action, and more – to enhance usability and engagement.
 
@@ -201,7 +207,7 @@ const Web_Design_Service = () => {
 
 
 
-                            7. Design Handoff to Development: <br />
+                            <span className='font-montSerrat font-bold'>7. Design Handoff to Development:</span> <br />
 
                             What We Do: Once the design is finalized, we prepare all the design assets and specifications to hand off to the development phase.
 
